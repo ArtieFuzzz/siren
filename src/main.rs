a1a94ba56378a5ad8b61a90d8cff4207c159be9b
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2022-02-22 20:30:47 +1100

    Framework

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,91 @@
 extern crate dotenv;
 
-use dotenv::dotenv;
+use std::{collections::HashSet, env, sync::Arc};
+
 use serenity::{
     async_trait,
+    client::bridge::gateway::ShardManager,
+    framework::{
+        standard::{macros::hook, DispatchError},
+        StandardFramework,
+    },
+    http::Http,
     model::{channel::Message, gateway::Ready},
     prelude::*,
 };
-use std::env;
+use tracing::info;
+
+pub struct ShardManagerContainer;
+
+impl TypeMapKey for ShardManagerContainer {
+    type Value = Arc<Mutex<ShardManager>>;
+}
 
 struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-    async fn message(&self, ctx: Context, message: Message) {
-        if message.content == "!ping" {
-            if let Err(why) = message.channel_id.say(&ctx.http, "Pong!").await {
-                println!(
-                    "There was an error while running the ping command {:?}",
-                    why
-                )
-            }
-        }
-    }
-
     async fn ready(&self, _: Context, ready: Ready) {
-        println!("Connected as {}", ready.user.name);
+        info!("Logged in as {}!", ready.user.tag())
+    }
+}
+
+#[hook]
+async fn dispatch_error(ctx: &Context, msg: &Message, error: DispatchError) {
+    if let DispatchError::Ratelimited(info) = error {
+        // We notify them only once.
+        if info.is_first_try {
+            let _ = msg
+                .channel_id
+                .say(
+                    &ctx.http,
+                    &format!("Try this again in {} seconds.", info.as_secs()),
+                )
+                .await;
+        }
     }
 }
 
 #[tokio::main]
 async fn main() {
-    dotenv().ok();
+    dotenv::dotenv().expect("Could not load .env");
+
+    tracing_subscriber::fmt::init();
+
     let token = env::var("TOKEN").expect("TOKEN is not set in .env");
+    let http = Http::new_with_token(&token);
+
+    let (owners, _bot_id) = match http.get_current_application_info().await {
+        Ok(info) => {
+            let mut owners = HashSet::new();
+            owners.insert(info.owner.id);
+
+            (owners, info.id)
+        }
+        Err(why) => panic!("Could not access application info: {:?}", why),
+    };
+
+    let framework = StandardFramework::new().configure(|c| c.owners(owners).prefix("!!"));
 
     let mut client = Client::builder(&token)
+        .framework(framework)
         .event_handler(Handler)
         .await
         .expect("Client went brrr");
+
+    {
+        let mut data = client.data.write().await;
+        data.insert::<ShardManagerContainer>(client.shard_manager.clone());
+    }
+
+    let shard_manager = client.shard_manager.clone();
+
+    tokio::spawn(async move {
+        tokio::signal::ctrl_c()
+            .await
+            .expect("Could not register Termination handler");
+        shard_manager.lock().await.shutdown_all().await;
+    });
 
     if let Err(why) = client.start().await {
         println!("Client Err: {:?}", why);
